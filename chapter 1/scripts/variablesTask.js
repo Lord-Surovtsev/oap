11038df5c350b72a38b0b620ba862c5992193f89
Lord-Surovtsev <lord@ubuntu.(none)>
2013-10-29 06:12:56 +0400

    initial commit

---
+++ chapter 1/scripts/variablesTask.js
@@ -0,0 +1,6 @@
+﻿function copyingVariables_Surovtsev() {
+	var admin;
+	var name = "Василий";
+	admin = name;
+	alert(admin);
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("helloWorld").click(function(){
+		$(this).load("./index.html");
+		})
+});
